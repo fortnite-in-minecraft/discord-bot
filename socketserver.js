a1f90978a746fefbc253e4403efae804e11786c2
maxh65000 <booah8@gmail.com>
2019-06-03 10:53:44 -0400

    Fix the readline

--- socketserver.js
+++ socketserver.js
@@ -1,19 +1,21 @@
 const net = require("net");
 const fs = require("fs");
 const readline = require("readline");
+let rl;
 const unixServer = net.createServer(function(client) {
+    rl && rl.close();
     client.write(`["hello", {"A":0}]`);
+    rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+    });
+
+    rl.on("line", function(line){
+        client.write(line);
+    });
 });
 unixServer.listen("socket");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-    terminal: false
-});
-
-rl.on("line", function(line){
-    client.write(line);
-});
 
 try{
     fs.unlinkSync("socket");
