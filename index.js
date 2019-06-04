a1ab287f77581ba7bcfba0f3d1640e0307965de2
maxh65000 <booah8@gmail.com>
2019-06-03 20:50:34 -0400

    Working discord client (& testing server)

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const fs = require("fs");
 const net = require("net");
 const path = require("path");
 const config = require("./config.json");
+const channelData = require("./data.json")
 const token = config.token;
 const Discord = require("discord.js");
+const readline = require("readline");
 
-let sockClient, guild, adminLog, pointLog, playerLog;
+let sockClient, guild, adminLog, pointLog, playerLog, rl;
 
 const client = new Discord.Client();
 
@@ -15,22 +17,77 @@ client.on("ready", () => {
     pointLog = guild.channels.get(config.channels.pointLog);
     playerLog = guild.channels.get(config.channels.playerLog);
 
+    rl && rl.close();
     sockClient && sockClient.close();
+
     sockClient = net.createConnection("socket");
+    rl = readline.createInterface({
+        input: sockClient,
+        terminal: false
+    });
 
-
-    sockClient.on("data", function(line){
+    rl.on("line", function(line){
         try{
+            line = line.toString();
             const json = JSON.parse(line);
             console.log("parsed json", json);
             const event = json[0];
             const data = json[1];
+
+            let channel;
+            switch(channelData.mappings[event].logChannel){
+                case "adminLog":
+                    channel = adminLog;
+                    break;
+                case "pointLog":
+                    channel = pointLog;
+                    break;
+                case "playerLog":
+                    channel = playerLog;
+                    break;
+                default:
+                    throw new Error("Unrecognized type " + event);
+            }
+
+            let message;
             switch(event){
                 case "logLine":
-                    playerLog.send(data.line);
+                    message = data.line;
+                    break;
+                case "playerDeath":
+                    message = `${data.player.user} ${data.cause}`;
+                    break;
+                case "playerKill":
+                    message = `${data.player.user} eliminated ${data.killer.user} ${data.cause}`;
+                    break;
+                case "playerDeathDueToOtherPlayer":
+                    message = `${data.player.user} eliminated ${data.killer.user} ${data.cause}`;
+                    break;
+                case "pointChange":
+                    message = "";
+                    break;
+                case "anticheatOffense":
+                    message = `${data.player.user} ${data.desc}`;
+                    break;
             }
+
+            channel.send({
+                "embed": {
+                    "title": channelData.mappings[event].desc,
+                    "description": message,
+                    "url": "https://discordapp.com",
+                    "color": 16777215,
+                    "timestamp": "2019-06-03T23:57:02.367Z",
+                    "footer": {
+                        "text": "Raw json payload: " + JSON.stringify(data)
+                    },
+                    "author": {
+                        "name": event
+                    }
+                }
+            });
         }catch (e) {
-            console.error(e);
+            console.error(e, line);
         }
     });
 
